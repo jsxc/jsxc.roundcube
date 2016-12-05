d4771a7bb89b6faa5134afad0ceff62f5df10bb1
Dmitry Telegin <mitya@cargosoft.ru>
2016-12-05 16:08:38 +0300

    Rework to use render_page hook

--- jsxc.php
+++ jsxc.php
@@ -1,84 +1,91 @@
 <?php
 class jsxc extends rcube_plugin
 {
-	private $user="";
-	private $passwd="";
-	private $xmpp_url="";
-	private $xmpp_domain="";
-	private $xmpp_resource="";
-	private $xmpp_overwrite=True;
-	private $jsxc_root="/";
-	private $jsxc_load_jquery=False;
+    private $rcmail;
+    private $xmpp_domain="";
+    private $xmpp_resource="";
+    private $xmpp_overwrite=True;
+    private $jsxc_root="/";
+    private $jsxc_load_jquery=False;
 
+    function init() {
 
-	function init(){
-		$rcmail = rcmail::get_instance();
-	        $this->load_config();
-	        $this->xmpp_bosh_url = $rcmail->config->get('xmpp_bosh_url');
-	        $this->xmpp_domain = $rcmail->config->get('xmpp_domain');
-	        $this->xmpp_resource = $rcmail->config->get('xmpp_resource');
-		$this->xmpp_overwrite = $rcmail->config->get('xmpp_overwrite');
-		$this->jsxc_root = $rcmail->config->get('jsxc_root');
-		$this->jsxc_load_jquery = $rcmail->config->get('jsxc_load_jquery');
+        $rcmail = rcmail::get_instance();
+        $this->rcmail = $rcmail;
 
-                $this->add_hook('storage_connect', array($this, 'carga_chat'));
-	}
+        $this->load_config();
+        $this->xmpp_bosh_url = $rcmail->config->get('xmpp_bosh_url');
+        $this->xmpp_domain = $rcmail->config->get('xmpp_domain');
+        $this->xmpp_resource = $rcmail->config->get('xmpp_resource');
+        $this->xmpp_overwrite = $rcmail->config->get('xmpp_overwrite');
+        $this->jsxc_root = $rcmail->config->get('jsxc_root');
+        $this->jsxc_load_jquery = $rcmail->config->get('jsxc_load_jquery');
 
-	function render_page ($args){
+        $this->add_hook('render_page', array($this, 'on_render_page'));
 
-                $this->api->output->add_script("
+    }
 
-			$(function() {
-   				jsxc.init({
-					logoutElement: $('#rcmbtn101'),
-					checkFlash: false,
-				      	rosterAppend: 'body',
-				      	root: '".$this->jsxc_root."plugins/jsxc',
-				      	turnCredentialsPath: 'plugins/jsxc/ajax/getturncredentials.json',
-				      	displayRosterMinimized: function() {
-				        	return true;
-				    	},
-					otr: {
-				      		debug: true,
-				      		SEND_WHITESPACE_TAG: true,
-				      		WHITESPACE_START_AKE: true
-					},
-    					loadSettings: function(username, password) {
-         					return {
-            						xmpp: {
-               							url: '".$this->xmpp_bosh_url."',
-               							domain: '".$this->xmpp_domain."',
-						               	resource: '".$this->xmpp_resource."',
-               							overwrite: ".$this->xmpp_overwrite.",
-               							onlogin: false,
-            						}
-         					};
-      					}
-   				});
-			});
-                ",'foot');
+    function load_jsxc() {
 
-                //Cargo las hojas CSS
-                $this->include_stylesheet('css/jquery-ui.min.css');
-                $this->include_stylesheet('css/jsxc.css');
+        // Load CSS stylesheets
+        $this->include_stylesheet('css/jquery-ui.min.css');
+        $this->include_stylesheet('css/jsxc.css');
 
-                //Cargo los archivos JS
-		if ($this->jsxc_load_jquery){
-                	$this->include_script('lib/jquery.min.js');
-		}
-                $this->include_script('lib/jquery.ui.min.js');
-                $this->include_script('lib/jquery.slimscroll.js');
-                $this->include_script('lib/jquery.fullscreen.js');
-                $this->include_script('lib/jsxc.dep.js');
-                $this->include_script('jsxc.min.js');
-	}
+        // Load JS modules
+        if ($this->jsxc_load_jquery) {
+            $this->include_script('lib/jquery.min.js');
+        }
+        $this->include_script('lib/jquery.ui.min.js');
+        $this->include_script('lib/jquery.slimscroll.js');
+        $this->include_script('lib/jquery.fullscreen.js');
+        $this->include_script('lib/jsxc.dep.min.js');
+        $this->include_script('jsxc.min.js');
 
-	function carga_chat($args) {
-		$this->user=$args['user'];
-		$this->passwd=$args['password'];
-		if ((!empty($this->user))&&(!empty($this->passwd))){
-			$this->add_hook('render_page', array($this, 'render_page'));
-		}
-	}
+        $this->api->output->add_script("
+
+            $(function() {
+                jsxc.init({
+                    loginForm: {
+                        form: '#login-form > div > form',
+                        jid: '#rcmloginuser',
+                        pass: '#rcmloginpwd'
+                    },
+                    rosterAppend: 'body',
+                    root: '".$this->jsxc_root."plugins/jsxc',
+                    displayRosterMinimized: function() {
+                        return true;
+                    },
+                    xmpp: {
+                        url: '".$this->xmpp_bosh_url."',
+                        domain: '".$this->xmpp_domain."',
+                        resource: '".$this->xmpp_resource."',
+                        overwrite: ".$this->xmpp_overwrite.",
+                        onlogin: false
+                    }
+                });
+            });
+        ", 'foot');
+
+    }
+
+    function on_render_page($args) {
+
+        switch($args['template']) {
+
+            case 'login':
+            case 'mail':
+            case 'compose':
+            case 'addressbook':
+            case 'settings':
+            case 'folders':
+            case 'identities':
+            case 'responses':
+            case 'about':
+                $this->load_jsxc();
+                break;
+
+        }
+
+    }
+
 }
-?>
